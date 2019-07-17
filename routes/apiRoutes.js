a97dddfa8ed26035da803b4b033a239abba75e2a
Katie Lewis <klewi314@gmail.com>
2019-07-16 19:10:24 -0700

    added get request to api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const axios = require('axios')
 const db = require('../models')
 const path = require('path')
+const { apiKey } = require('../keys.js')
 
 module.exports = app => {
 
   //Retrieve based on search input
   app.get('/api/books', (req, res) => {
-    axios.get(`https://www.googleapis.com/books/v1/volumes?q=flowers&key=${process.env.KEY}
-`)
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=harrypotter&country=US&key=${apiKey}`)
       .then(res => console.log(res)
       )
       .catch(e => console.log(e))
